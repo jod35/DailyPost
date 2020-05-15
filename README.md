59763cfd4d0a07360a18575e3f2f3d72f3f4284e
jod35 <jod35@gmail.com>
2020-05-15 14:32:27 +0300

    initial commmit

---
+++ README.md
@@ -0,0 +1,24 @@
+# DailyPost 
+DailyPost is a bloggging company. It requires a website 
+- where the journalists are able to write their posts, edit them and delete them as well.
+
+- Users of DailyPost should be able to comment on posts.
+
+- The site should have an admin who works on the entire webapp.
+
+## Technologies Used
+The project was built using Python3 and the following technologies
+- Django
+- SQLite Database for development 
+- HTML CSS and Javascript in the front-end
+
+
+
+## Running the Application
+
+### 1. Clone the repo
+`
+
+##  2.Run the application 
+` 
+
---
+++ user/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+posts=[
+    {
+        "author":"Ssali Jonathan",
+        "title":"Bored In the House",
+        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa corrupti sint at consequuntur ab non! Aliquam est, pariatur quae explicabo possimus sequi Quaerat iure veniam dolores! Totam impedit explicabo recusandae!"
+    }
+    ,
+      {
+        "author":"Nassali Jannet",
+        "title":"How to make an apple cake",
+        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa corrupti sint at consequuntur ab non! Aliquam est, pariatur quae explicabo possimus sequi Quaerat iure veniam dolores! Totam impedit explicabo recusandae!"
+    },
+      {
+        "author":"Mirembe Jerusha",
+        "title":"5 ways to control spread of Corona Virus",
+        "content":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa corrupti sint at consequuntur ab non! Aliquam est, pariatur quae explicabo possimus sequi Quaerat iure veniam dolores! Totam impedit explicabo recusandae!"
+    }
+]
+
+def index(request):
+    context={"posts":posts,"title":"Home Page"}
+    return render(request,'user/index.html',context)
+
+
+def about(request):
+    return render(request,'user/about.html')
